8f6dc8c22ed0f85378b8c4761beba4858dbe4446
yeti-teti <yetit1762@gmail.com>
2025-05-16 22:55:51 -0700

    tf

--- Terraform/dev/main.tf
+++ Terraform/dev/main.tf
@@ -223,7 +223,8 @@ resource "google_container_cluster" "primary" {
   }
 
   private_cluster_config {
-    enable_private_nodes    = true
+    # enable_private_nodes    = true
+    enable_private_nodes    = false
     enable_private_endpoint = false
     master_ipv4_cidr_block  = "10.10.30.0/28"
   }
